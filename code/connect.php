8ba1b02a619e15b89552e893861b0045b58c0dfd
cphuamao <b06705058@ntu.edu.tw>
2020-06-08 19:17:35 +0800

    <version1> add connect to database

---
+++ code/connect.php
@@ -0,0 +1,30 @@
+<?php
+
+$db_server = "localhost";
+
+$db_name = "mydb";
+
+$db_user = "root";
+
+$db_passwd = "1230";
+
+$mysqli = new mysqli("localhost", "root", "1230", "mydb");
+
+
+if ($mysqli -> connect_error) {
+    die("連線失敗: " . $mysqli->connect_error);
+}
+//echo "連線成功";
+$mysqli->query("SET NAMES 'utf8'");
+/*
+if(!@mysqli_connect($db_server, $db_user, $db_passwd))
+	die("無法對資料庫連線");
+
+mysqli_query("SET NAMES utf8");
+
+if(!@mysqli_select_db($db_name))
+        die("無法使用資料庫");
+*/
+
+
+?> 
