9b33f9519bb1b61751ddde35e2fbb530608e287c
Sam <wang.sam33@gmail.com>
2020-06-10 02:42:06 +0800

    add register.php

--- code/logout_db.php
+++ code/logout_db.php
@@ -45,7 +45,7 @@
         <div class="title">
           <h1 class="heading">Tutor</h1>
         </div>
-        <h5><kbd>team seven</kbd></h5>
+        <h5><kbd>team eight</kbd></h5>
         <!-- <p class="lead my-5">台大學生設計的家教媒合網站</p> -->
 
         <div class="content my-5 py-3 p-sm-4">
@@ -62,7 +62,7 @@
         </div>
       </div>
     </div>
-      	
+        
   <div class="img-thumbnail">
       <div class="text-center">
         <h1>
@@ -102,7 +102,7 @@
     
 
 
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
